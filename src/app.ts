a94208505e6b0de606f3970d8811e68c34ee6ba0
Fendy Heyanto <fendy3002@gmail.com>
2019-10-15 13:28:08 +0700

    Added test

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 require('dotenv').config();
 import express = require('express');
 import routes from './routes';
+import {healthcheck} from '@fendy3002/express-helper';
 
 const init = async () => {
     let app = express();
     app.use(await routes(app));
-    
+    app.use(await healthcheck({
+        check: {
+            mysql: async () => { return; }
+        },
+        checkTimeout: 1000
+    }));
+
     return {
         app: app
     };
--- test/_/index.ts
+++ test/_/index.ts
@@ -2,11 +2,21 @@ import * as mocha from 'mocha';
 // assert cannot be defaulted import
 import assert = require('assert');
 import addContext = require('mochawesome/addContext');
+import chai = require('chai');
+import chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+import app from '../../src/app';
 
 mocha.describe("", function(this) {
-  mocha.it("should assert true", function(){
-    let actual = true;
-    assert.equal(true, actual);
-    addContext(this, 'simple string');
+  mocha.it("should 204 healthcheck", async function(){
+    let initApp = await app();
+    let response = await chai.request(initApp.app)
+      .get('/~/health');
+    addContext(this, {
+      title: "res.status",
+      value: response.status
+    });
+    assert.equal(204, response.status);
   })
 })
